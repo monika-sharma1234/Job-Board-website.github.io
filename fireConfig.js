007d9b66ef2303595f8afe0f27fdb3f085b47ab5
Monika <monikasharma9411441908@gmail.com>
2022-11-22 13:10:28 +0530

    10

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDK47UxS84kR4wmcx6lNHq1oPaMcGm29aI",
+    authDomain: "myfirstfirebase-b597c.firebaseapp.com",
+    databaseURL: "https://myfirstfirebase-b597c-default-rtdb.firebaseio.com",
+    projectId: "myfirstfirebase-b597c",
+    storageBucket: "myfirstfirebase-b597c.appspot.com",
+    messagingSenderId: "56509967708",
+    appId: "1:56509967708:web:8d38d1ebd154e272b7a599"
+  };
+  //Initialize Firebase
+  firebaseConfig.initializeApp()
